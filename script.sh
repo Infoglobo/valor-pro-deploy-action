cfee912f4be2ec834c6beb5e726c196a3e44c1fa
Mauricio Gamarra <mauricio.gamarra.gbi@edglobo.com.br>
2022-12-23 13:09:22 -0300

    slack integration

--- script.sh
+++ script.sh
@@ -143,5 +143,5 @@ D=$(date '+%d-%m-%Y %H:%M')
 set -x
 
 
-curl -v -X POST -H 'Content-type: application/json' --data '{"text": "Aplicação *'$GITHUB_REPOSITORY'* deployada no ambiente *'$AMBIENTE'* por *'$GITHUB_ACTOR'* em '$D'.", "icon_emoji": ":rocket:"}' https://hooks.slack.com/services/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
+curl -v -X POST -H 'Content-type: application/json' --data '{"text": "Aplicação *'$GITHUB_REPOSITORY'* deployada no ambiente *'$AMBIENTE'* por *'$GITHUB_ACTOR'* em '$D'.", "icon_emoji": ":rocket:"}' https://hooks.slack.com/services/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
 
